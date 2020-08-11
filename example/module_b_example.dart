f6cd187054f565e701b69c9e54966cefa05d77c2
es <es@MacBook-Pro-mac.local>
2020-08-12 01:43:29 +0300

    init

---
+++ example/module_b_example.dart
@@ -0,0 +1,6 @@
+import 'package:module_b/module_b.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/module_b_test.dart
@@ -0,0 +1,16 @@
+import 'package:module_b/module_b.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    Awesome awesome;
+
+    setUp(() {
+      awesome = Awesome();
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
