b127bf3e53a0352971e5efcfa3ea05737ba312b7
es <es@MacBook-Pro-mac.local>
2020-08-12 01:58:54 +0300

    Changes

--- lib/src/module_b_base.dart
+++ lib/src/module_b_base.dart
@@ -1,6 +1,5 @@
-// TODO: Put public facing types in this file.
-
-/// Checks if you are awesome. Spoiler: you are.
-class Awesome {
-  bool get isAwesome => true;
+class ModuleB {
+  static void sayHello() {
+    print('I\'am module module_b');
+  }
 }
